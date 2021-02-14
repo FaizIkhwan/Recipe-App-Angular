83881a7202d3eb32766b83f75abb5a8e0af0c2bd
FaizIkhwan <faiz.ikhwan@hexainitiative.com>
2021-02-15 00:55:08 +0800

    finished chapter 12

--- recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,13 +10,31 @@ import {RecipeService} from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          const stringConstantId = 'id';
+          this.id = +params[stringConstantId];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
+  }
 
   onAddToShoppingList(): void {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(): void {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
