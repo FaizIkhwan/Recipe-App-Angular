6bcb48dd85c57afdac46b328c4e01b2b3c32a8c3
FaizIkhwan <faiz.ikhwan@hexainitiative.com>
2021-02-21 14:18:28 +0800

    finished chapter 19

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -9,27 +9,34 @@ export class RecipeService {
 
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Spaghetti',
-      'Spaghetti Sedap',
-      'https://www.seriouseats.com/2020/03/20200224-carretteira-pasta-vicky-wasik-21.jpg',
-      [new Ingredient('Meat', 1)]
-    ),
-    new Recipe(
-      'Mushroom Soup',
-      'Mushroom Soup Sedap',
-      'https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/1506455983-delish-cream-mushroom-soup.jpg',
-      [new Ingredient('Bun', 2)]
-    ),
-    new Recipe('Chicken',
-      'Chicken Sedap',
-      'https://www.onceuponachef.com/images/2020/05/best-grilled-chicken-scaled.jpg',
-      [new Ingredient('Fries', 3)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Spaghetti',
+  //     'Spaghetti Sedap',
+  //     'https://www.seriouseats.com/2020/03/20200224-carretteira-pasta-vicky-wasik-21.jpg',
+  //     [new Ingredient('Meat', 1)]
+  //   ),
+  //   new Recipe(
+  //     'Mushroom Soup',
+  //     'Mushroom Soup Sedap',
+  //     'https://hips.hearstapps.com/del.h-cdn.co/assets/17/39/1506455983-delish-cream-mushroom-soup.jpg',
+  //     [new Ingredient('Bun', 2)]
+  //   ),
+  //   new Recipe('Chicken',
+  //     'Chicken Sedap',
+  //     'https://www.onceuponachef.com/images/2020/05/best-grilled-chicken-scaled.jpg',
+  //     [new Ingredient('Fries', 3)]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]): void {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
